e1c2407f5ffbaf1aeb16178688a8c89b7b206079
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2025-01-20 11:36:20 +0000

    Deploy infrastructure into existing environment
    * This enables us to make use of existing infrastructure that is deployed and managed outside of this repository (academies-api)

--- terraform/container-apps-hosting.tf
+++ terraform/container-apps-hosting.tf
@@ -1,12 +1,10 @@
 module "azure_container_apps_hosting" {
-  source = "github.com/DFE-Digital/terraform-azurerm-container-apps-hosting?ref=v1.16.3"
+  source = "github.com/DFE-Digital/terraform-azurerm-container-apps-hosting?ref=v1.16.4"
 
   environment    = local.environment
   project_name   = local.project_name
   azure_location = local.azure_location
   tags           = local.tags
-
-  virtual_network_address_space = local.virtual_network_address_space
 
   enable_container_registry             = local.enable_container_registry
   registry_admin_enabled                = local.registry_admin_enabled
@@ -21,11 +19,22 @@ module "azure_container_apps_hosting" {
   dns_mx_records       = local.dns_mx_records
 
   image_name                             = local.image_name
+  image_tag                              = local.image_tag
   container_command                      = local.container_command
   container_secret_environment_variables = local.container_secret_environment_variables
   container_scale_http_concurrency       = local.container_scale_http_concurrency
   container_apps_allow_ips_inbound       = local.container_apps_allow_ips_inbound
   container_min_replicas                 = local.container_min_replicas
+  container_port                         = local.container_port
+  enable_health_insights_api             = local.enable_health_insights_api
+  health_insights_api_cors_origins       = local.health_insights_api_cors_origins
+  health_insights_api_ipv4_allow_list    = local.health_insights_api_ipv4_allow_list
+
+  existing_container_app_environment = local.existing_container_app_environment
+  existing_virtual_network           = local.existing_virtual_network
+  existing_resource_group            = local.existing_resource_group
+  launch_in_vnet                     = false
+  container_app_name_override        = "persons-api"
 
   enable_cdn_frontdoor                            = local.enable_cdn_frontdoor
   cdn_frontdoor_forwarding_protocol               = local.cdn_frontdoor_forwarding_protocol
@@ -36,6 +45,8 @@ module "azure_container_apps_hosting" {
   cdn_frontdoor_custom_domains                    = local.cdn_frontdoor_custom_domains
   cdn_frontdoor_origin_fqdn_override              = local.cdn_frontdoor_origin_fqdn_override
   cdn_frontdoor_origin_host_header_override       = local.cdn_frontdoor_origin_host_header_override
+  enable_cdn_frontdoor_vdp_redirects              = local.enable_cdn_frontdoor_vdp_redirects
+  cdn_frontdoor_vdp_destination_hostname          = local.cdn_frontdoor_vdp_destination_hostname
   cdn_frontdoor_waf_custom_rules                  = local.cdn_frontdoor_waf_custom_rules
   enable_cdn_frontdoor_health_probe               = local.enable_cdn_frontdoor_health_probe
 
@@ -48,10 +59,5 @@ module "azure_container_apps_hosting" {
   container_health_probe_path     = local.container_health_probe_path
   cdn_frontdoor_health_probe_path = local.cdn_frontdoor_health_probe_path
   monitor_endpoint_healthcheck    = local.monitor_endpoint_healthcheck
-
-  existing_logic_app_workflow                  = local.existing_logic_app_workflow
-  existing_network_watcher_name                = local.existing_network_watcher_name
-  existing_network_watcher_resource_group_name = local.existing_network_watcher_resource_group_name
-
-  custom_container_apps = local.custom_container_apps
+  existing_logic_app_workflow     = local.existing_logic_app_workflow
 }
